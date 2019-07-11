1630b7db3871b3ad902e7fae3d6f5bf766483cfc
Susheel Nyamala <snyamala@codeaurora.org>
2019-07-11 21:13:56 +0530

    Fix data deactivate issue due to intermediate network requests
    During temporary DDS switch triggered for calls on non DDS sub, TRACK_DEFAULT
    internet network requests are received followed by release requests due to
    which default apncontext disable is triggered leading to disconnect event.
    Default data call is deactivated as soon as it is setup due to this disconnect.
    
    Fix is to allow only REQUEST type network requests and skip other types
    
    Change-Id: Ie07718289e8b4439e957550a7b6f8be53b24b9a6
    CRs-Fixed: 2438620

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -668,6 +668,11 @@ public class PhoneSwitcher extends Handler {
 
     private void onRequestNetwork(NetworkRequest networkRequest) {
         final DcRequest dcRequest = new DcRequest(networkRequest, mContext);
+        if (networkRequest.type != NetworkRequest.Type.REQUEST) {
+           log("Skip non REQUEST type request - " + networkRequest);
+           return;
+        }
+
         if (!mPrioritizedDcRequests.contains(dcRequest)) {
             collectRequestNetworkMetrics(networkRequest);
             mPrioritizedDcRequests.add(dcRequest);
@@ -679,7 +684,8 @@ public class PhoneSwitcher extends Handler {
     private void onReleaseNetwork(NetworkRequest networkRequest) {
         final DcRequest dcRequest = new DcRequest(networkRequest, mContext);
 
-        if (mPrioritizedDcRequests.remove(dcRequest)) {
+        if (mPrioritizedDcRequests.contains(dcRequest) &&
+                mPrioritizedDcRequests.remove(dcRequest)) {
             onEvaluate(REQUESTS_CHANGED, "netReleased");
             collectReleaseNetworkMetrics(networkRequest);
         }
