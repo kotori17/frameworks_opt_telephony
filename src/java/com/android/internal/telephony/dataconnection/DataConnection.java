phhusson/platform_frameworks_opt_telephony

Add route to gateway on null netmask
Galaxy S9 returns empty prefix

Change-Id: I8133da8e8f5203666684100009ba658528a02e94

src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.ConnectivityManager.PacketKeepalive;
 import android.net.KeepalivePacketData;
+import android.net.IpPrefix;
 import android.net.LinkAddress;
 import android.net.LinkProperties;
 import android.net.NetworkAgent;
@@ -1127,6 +1128,22 @@ public class DataConnection extends StateMachine {
                 }
 
                 for (InetAddress gateway : response.getGateways()) {
+                    if(response.getAddresses().size()>0) {
+                        //TODO:
+                        // - IPv6
+                        // - Multiple addresses
+                        // - Check for non-trivial prefix length
+                        LinkAddress la = response.getAddresses().get(0);
+                        if(la.getNetworkPrefixLength() == 32 &&
+                            gateway instanceof java.net.Inet4Address) {
+                            if(!gateway.isAnyLocalAddress()) {
+                                linkProperties.addRoute(new RouteInfo(
+                                            new IpPrefix(gateway, 32),
+                                            InetAddress.getByName("0.0.0.0"),
+                                            response.getIfname()));
+                            }
+                        }
+                    }
                     // Allow 0.0.0.0 or :: as a gateway;
                     // this indicates a point-to-point interface.
                     linkProperties.addRoute(new RouteInfo(gateway));

kotori17/frameworks_opt_telephony

src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -25,6 +25,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.KeepalivePacketData;
+import android.net.IpPrefix;
 import android.net.LinkAddress;
 import android.net.LinkProperties;
 import android.net.NetworkCapabilities;
@@ -1429,6 +1430,22 @@ public class DataConnection extends StateMachine {
                 }
 
                 for (InetAddress gateway : response.getGatewayAddresses()) {
+                    if(response.getAddresses().size()>0) {
+                        //TODO:
+                        // - IPv6
+                        // - Multiple addresses
+                        // - Check for non-trivial prefix length
+                        LinkAddress la = response.getAddresses().get(0);
+                        if(la.getNetworkPrefixLength() == 32 &&
+                            gateway instanceof java.net.Inet4Address) {
+                            if(!gateway.isAnyLocalAddress()) {
+                                linkProperties.addRoute(new RouteInfo(
+                                            new IpPrefix(gateway, 32),
+                                            InetAddress.getByName("0.0.0.0"),
+                                            response.getInterfaceName()));
+                            }
+                        }
+                    }
                     // Allow 0.0.0.0 or :: as a gateway;
                     // this indicates a point-to-point interface.
                     linkProperties.addRoute(new RouteInfo(gateway));